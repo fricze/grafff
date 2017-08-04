f27049e3d69c221ae9d2c3a3f7b47bee6041348b
Andrzej Fricze <andrzej.fricze@functionite.com>
2017-08-04 12:45:13 +0200

    Reduce them then!

--- grafff.clj
+++ grafff.clj
@@ -10,7 +10,21 @@
   ([a b] (start-with-edge a b))
   ([g a b] (add-edge g a b)))
 
-(->
+(def g (->
  (edge :vertex 90)
  (edge :vertex 80)
- (edge :vertex 70))
+ (edge :vertex 70)
+
+ (edge 70 100)
+ (edge 100 120)
+ (edge 120 4)
+ (edge 120 6)
+ (edge 120 8)))
+
+(defn travel [g v mem]
+  (if (contains? g v)
+    (reduce
+     #(flatten (conj %1 %2))
+     (conj mem v)
+     (map #(travel g % mem) (g v)))
+    v))
