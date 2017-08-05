f0be319e92eb57362f97a99d0ec1264a2e10386e
Andrzej Fricze <andrzej.fricze@functionite.com>
2017-08-05 18:47:35 +0200

    Well, it takes me somwhere

--- grafff.clj
+++ grafff.clj
@@ -13,13 +13,37 @@
 (def g (->
         (edge 70 100)
         (edge 100 120)
+        (edge 100 130)
+        (edge 100 140)
         (edge 120 8)
         (edge 8 70)))
 
-(defn travel [g v mem]
-  (if (and (not (some #{v} mem)) (contains? g v))
-    (reduce
-     #(flatten (conj %1 %2))
-     mem
-     (map #(travel g % (conj mem v)) (g v)))
-    v))
+(defn travel-1 [g v been-there]
+  (cond
+    ;; if g doesn't contain v it means node v doesn't
+    ;; have any edge going out, so we're just adding
+    ;; it to set
+    (not (contains? g v)) [v]
+    (and
+     (contains? g v)
+     (not ((into #{} been-there) v)))
+    (let [been-there (conj been-there v)] (reduce
+                                           #(clojure.set/union %1 %2)
+                                           been-there
+                                           (map #(travel-1 g % been-there) (g v))))
+    (and
+     (contains? g v)
+     ((into #{} been-there) v)) [v]))
+
+(println "--------------")
+
+(travel-1 g 70 [])
+(travel-1 g 70 #{})
+
+;; ->
+;; (edge 70 100)
+;; (edge 100 120)
+;; (edge 100 130)
+;; (edge 100 140)
+;; (edge 120 8)
+;; (edge 8 70)
